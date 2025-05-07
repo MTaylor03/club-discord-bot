727fb327f212f77769c172fec946b81a95d2aa2f
Matt Taylor <mt939121@ohio.edu>
2025-05-06 20:58:59 -0400

    Crash fix

--- commands/faq.js
+++ commands/faq.js
@@ -1,8 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 
-const faq = JSON.parse( 
-    fs.readFileSync(path.join(__dirname, 'faq.txt'), 'utf-8'));
+const faq = fs.readFileSync(path.join(__dirname, 'faq.txt'), 'utf-8');
 
 module.exports = {
     name: "faq",
