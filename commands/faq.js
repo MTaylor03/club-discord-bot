213cff7d42ad4dab742475850bca680d07ca75b9
Matt Taylor <mt939121@ohio.edu>
2025-05-06 20:57:07 -0400

    Hotfix

--- commands/faq.js
+++ commands/faq.js
@@ -2,16 +2,12 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 const faq = JSON.parse( 
-    fs.readFileSync(path.join(__dirname, 'faq.json'), 'utf-8'));
+    fs.readFileSync(path.join(__dirname, 'faq.txt'), 'utf-8'));
 
 module.exports = {
     name: "faq",
 
-    execute(message) {
-        for(const i of faq.questions)
-        {
-            message.channel.send(`Q: ${i.question}`);
-            message.channel.send(`A: ${i.answer}`);
-        }
+    async execute(message) {
+        message.channel.send(faq);
     }
 }
