5b1f34ef445c4685895e5eabfb7be4a685daba4d
Matt Taylor <mt939121@ohio.edu>
2025-05-06 20:24:42 -0400

    test

--- commands/shrug.js
+++ commands/shrug.js
@@ -1,11 +1,7 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { execute } = require('./fact');
-
 module.exports = {
     name: "shrug",
 
     async execute(message){
-        message.channel.send("/shrug");
+        message.channel.send("@here");
     },
 };
