6ef3e9d5a8e99b82a459639bbddb4bb2e2c5db96
Matt Taylor <mt939121@ohio.edu>
2025-05-08 08:44:57 -0400

    Rollback

--- commands/faq.js
+++ commands/faq.js
@@ -1,15 +1,12 @@
-const { SlashCommandBuilder } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
 
 const faq = fs.readFileSync(path.join(__dirname, 'faq.txt'), 'utf-8');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName(faq)
-        .setDescription('Print FAQ\'s'),
+    name: "faq",
 
     async execute(message) {
-        await message.reply({content: faq, ephemeral: true});
+        message.channel.send(faq);
     }
-};
+}
